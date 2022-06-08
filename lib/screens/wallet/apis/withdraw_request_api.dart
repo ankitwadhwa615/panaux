4fd4f356596692e22cd86a0e8c92a394b134e51e
ankitwadhwa615 <ankitwadhwa615@gmail.com>
2022-06-08 16:19:07 +0530

    wallet

--- lib/screens/wallet/apis/withdraw_request_api.dart
+++ lib/screens/wallet/apis/withdraw_request_api.dart
@@ -7,11 +7,13 @@ import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:panaux_customer/commons/api_constants.dart';
 import 'package:panaux_customer/screens/wallet/controllers/wallet_controller.dart';
+import '../../../commons/models/razorpay_order_model.dart';
 
 WalletController controller = Get.put(WalletController());
-Future withdrawalRequestApi({
+Future<RazorpayOrderModel?> withdrawalRequestApi({
   required int amount,
 }) async {
+  RazorpayOrderModel? razorpayOrderModel;
   var dio = Dio();
   try {
     var api = API.addMoneyToWallet;
@@ -22,9 +24,43 @@ Future withdrawalRequestApi({
     var response =
         await dio.post(api, data: jsonEncode(params), options: options);
 
-    print(response);
     if (response.data['status'] == "success") {
-     // Fluttertoast.showToast(msg: response.data['message']);
+      var responseData = json.encode(response.data["razorpayOrder"]);
+      razorpayOrderModel = razorpayOrderModelFromJson(responseData);
+    } else {
+      Fluttertoast.showToast(msg: response.data['message']);
+    }
+  } on DioError catch (e) {
+    if (kDebugMode) {
+      print(e);
+    }
+    Fluttertoast.showToast(msg: "Something went wrong");
+  } on SocketException {
+    Fluttertoast.showToast(msg: "Please check your internet connection");
+  }
+  return razorpayOrderModel;
+}
+Future verifyRazorpayTransactionApi(
+    String orderId,
+    String paymentId,
+    String signature
+    ) async {
+  var dio = Dio();
+  try {
+    var api = API.addMoneyToWallet;
+    var box = await Hive.openBox("userBox");
+    String token = box.get('token');
+    var options = Options(headers: {"Authorization": "Bearer " + token});
+    var params = {
+      "razorpay_order_id": orderId,
+      "razorpay_payment_id": paymentId,
+      "razorpay_signature": signature
+    };
+
+    var response =
+    await dio.patch(api, options: options, data: jsonEncode(params));
+    if (response.data["status"] == "success") {
+      Fluttertoast.showToast(msg: 'Added Money Successfully');
       controller.getBalance();
     } else {
       Fluttertoast.showToast(msg: response.data['message']);
@@ -38,3 +74,4 @@ Future withdrawalRequestApi({
     Fluttertoast.showToast(msg: "Please check your internet connection");
   }
 }
+
